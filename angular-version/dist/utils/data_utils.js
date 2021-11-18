51a23b473cce40746ff323e5c764d39adfb3d506
Federico M. Facca <federico.facca@gmail.com>
2021-11-18 16:05:14 +0100

    add angular version distribution

---
+++ angular-version/dist/utils/data_utils.js
@@ -0,0 +1,178 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DataFormatter = void 0;
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+// import { isEqual, reduce, filter, transform, isObject } from 'lodash';
+var DataFormatter =
+/*#__PURE__*/
+function () {
+  function DataFormatter() {
+    _classCallCheck(this, DataFormatter);
+  }
+
+  _createClass(DataFormatter, [{
+    key: "getValues",
+    value: function getValues(series) {
+      if (!series || series.length === 0) return {};
+
+      var seriesType = this._getSeriesType(series);
+
+      return seriesType === 'table' ? this._getSeries(series) : this._getSeriesTimeSeries(series);
+    }
+  }, {
+    key: "isJsonString",
+    value: function isJsonString(item) {
+      if (typeof item !== "string") {
+        return false;
+      }
+
+      try {
+        item = JSON.parse(item);
+      } catch (e) {
+        return false;
+      }
+
+      if (_typeof(item) === "object" && item !== null) {
+        return true;
+      }
+
+      return false;
+    }
+  }, {
+    key: "fixType",
+    value: function fixType(item) {
+      if (this.isJsonString(item)) {
+        return JSON.parse(item);
+      } else {
+        return item;
+      }
+    }
+  }, {
+    key: "_getSeries",
+    value: function _getSeries(series) {
+      var _this = this;
+
+      var hashSeriesByLayerByKey = {};
+      var seriesLayer = null;
+      var id = null;
+      series.forEach(function (series_elem) {
+        var columns = series_elem.columns.map(function (elem) {
+          return elem.text;
+        });
+        var type_index = columns.length - 1;
+        var id_index = 1;
+
+        if (columns.indexOf("type") < 0) {
+          console.warn("Missing type alias in the query (required to define layers)");
+        } else {
+          type_index = columns.indexOf("type");
+        }
+
+        if (columns.indexOf("id") < 0) {
+          console.warn("Missing id alias in the query");
+        } else {
+          id_index = columns.indexOf("id");
+        }
+
+        series_elem.rows.forEach(function (series_elem_row) {
+          seriesLayer = series_elem_row[type_index];
+
+          if (!hashSeriesByLayerByKey[seriesLayer]) {
+            hashSeriesByLayerByKey[seriesLayer] = {};
+          }
+
+          id = series_elem_row[id_index];
+
+          if (!hashSeriesByLayerByKey[seriesLayer][id]) {
+            hashSeriesByLayerByKey[seriesLayer][id] = [];
+          }
+
+          var hashWithValues = {};
+          columns.forEach(function (elem, i) {
+            if (i !== columns.indexOf("time") && i !== columns.length - 1) // do not insert grafana field 'time' and the group by field
+              {
+                hashWithValues[elem] = _this.fixType(series_elem_row[i]);
+              }
+          });
+          hashSeriesByLayerByKey[seriesLayer][id].push(hashWithValues);
+        });
+      });
+      return hashSeriesByLayerByKey;
+    }
+  }, {
+    key: "_getSeriesTimeSeries",
+    value: function _getSeriesTimeSeries(series) {
+      var setSeries = {};
+      var setSeriesByLayer = {};
+      series.forEach(function (series_elem) {
+        var _series_elem$target$s = series_elem.target.split(': '),
+            _series_elem$target$s2 = _slicedToArray(_series_elem$target$s, 2),
+            seriesLayer = _series_elem$target$s2[0],
+            seriesFieldName = _series_elem$target$s2[1];
+
+        if (!setSeriesByLayer[seriesLayer]) {
+          setSeriesByLayer[seriesLayer] = [];
+        }
+
+        setSeriesByLayer[seriesLayer].push([seriesFieldName].concat(_toConsumableArray(series_elem.datapoints.map(function (elem) {
+          return elem[0];
+        }))));
+      }); // get one array and transform into a hash
+
+      var hashSeriesByLayerByKey = {};
+      Object.keys(setSeriesByLayer).forEach(function (layerName) {
+        if (!hashSeriesByLayerByKey[layerName]) hashSeriesByLayerByKey[layerName] = {};
+        var superArray = setSeriesByLayer[layerName];
+
+        for (var column = 1; column < superArray[0].length; column++) {
+          var result = {};
+
+          for (var line = 0; line < superArray.length; line++) {
+            result[superArray[line][0]] = superArray[line][column];
+          }
+
+          if (!hashSeriesByLayerByKey[layerName][result.id]) hashSeriesByLayerByKey[layerName][result.id] = [];
+          hashSeriesByLayerByKey[layerName][result.id].push(result);
+        }
+      });
+      return hashSeriesByLayerByKey;
+    }
+  }, {
+    key: "_getSeriesType",
+    value: function _getSeriesType(series) {
+      return series[0].type;
+    }
+  }]);
+
+  return DataFormatter;
+}();
+
+exports.DataFormatter = DataFormatter;
+//# sourceMappingURL=data_utils.js.map
---
+++ angular-version/dist/utils/map_utils.js
@@ -0,0 +1,159 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getCityCoordinates = getCityCoordinates;
+exports.getDataPointStickyInfo = getDataPointStickyInfo;
+exports.getSelectedCity = getSelectedCity;
+exports.geolocationOptions = void 0;
+
+var _lodash = require("lodash");
+
+var _config = _interopRequireDefault(require("app/core/config"));
+
+var _string = require("./string");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+/**
+* private functions
+*/
+function getDataPointStickyInfo(dataPoint, metricsTranslations) {
+  var stickyInfo = '<div class="stycky-popup-info">';
+  var bodyData = getDataPointDetails(dataPoint, ['geojson', 'id', 'type', 'created_at', 'longitude', 'latitude', 'name'], false);
+
+  if (dataPoint.name && Object.keys(bodyData).length > 1) {
+    stickyInfo += '<div class="head">' + dataPoint.name + '</div>';
+  } else if (dataPoint.id && Object.keys(bodyData).length > 1) {
+    stickyInfo += '<div class="head">' + dataPoint.id + '</div>';
+  } else if (Object.keys(bodyData).length === 1) {
+    stickyInfo += '<div class="head">' + Object.keys(bodyData)[0] + '</div>';
+  }
+
+  var bodyClass = 'popup-single-value';
+
+  if (Object.keys(bodyData).length > 1) {
+    bodyClass = 'popup-multiple-value';
+  } // body
+
+
+  stickyInfo += '<div class="body">';
+  stickyInfo += translate(bodyData, metricsTranslations, bodyClass).join('');
+  stickyInfo += '</div>'; // foot
+
+  var footData;
+
+  if (Object.keys(bodyData).length === 1 && dataPoint.name) {
+    footData = getDataPointDetails(dataPoint, ['created_at', 'name'], true);
+  } else {
+    footData = getDataPointDetails(dataPoint, ['created_at'], true);
+  }
+
+  var footClass = '';
+  stickyInfo += '<div class="foot">';
+  stickyInfo += translate(footData, metricsTranslations, footClass).join('');
+  stickyInfo += '</div>';
+  return stickyInfo;
+}
+
+function getDataPointDetails(dataPoint, skipkey, include) {
+  return include ? Object.keys(dataPoint).filter(function (key) {
+    return skipkey.includes(key);
+  }).reduce(function (obj, key) {
+    obj[key] = dataPoint[key];
+    return obj;
+  }, {}) : Object.keys(dataPoint).filter(function (key) {
+    return !skipkey.includes(key);
+  }).reduce(function (obj, key) {
+    obj[key] = dataPoint[key];
+    return obj;
+  }, {});
+}
+
+function objToString(obj) {
+  var str = '<div>';
+
+  for (var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      if (_typeof(obj[p]) === 'object') {
+        str += p + ': ' + objToString(obj[p]) + '<br/>';
+      } else {
+        str += p + ': ' + obj[p] + '<br/>';
+      }
+    }
+  }
+
+  str += '</div>';
+  return str;
+}
+
+function translate(filteredData, metricsTranslations, cssClass) {
+  var keys = Object.keys(filteredData);
+  var translatedValues = keys.map(function (dpKey) {
+    var dP = dpKey === 'created_at' ? new Date(filteredData[dpKey]).toLocaleString() : _typeof(filteredData[dpKey]) === 'object' ? objToString(filteredData[dpKey]) : typeof filteredData[dpKey] === 'boolean' ? filteredData[dpKey] : isNaN(filteredData[dpKey]) ? filteredData[dpKey] : Number.isInteger(filteredData[dpKey]) ? filteredData[dpKey] : parseFloat(filteredData[dpKey].toFixed(2));
+    var trans = metricsTranslations.filter(function (elem) {
+      return elem[0] === dpKey;
+    });
+    return {
+      'name': trans.length > 0 && trans[0][1] ? trans[0][1] : (0, _string.titleize)(dpKey),
+      'value': dP || '-',
+      'unit': trans.length > 0 ? trans[0][2] : ''
+    };
+  });
+  return translatedValues.map(function (translatedValue) {
+    return "<div class='".concat(cssClass, "'><span class='name'>").concat(translatedValue.name, "</span><span class='value'>").concat(translatedValue.value, "</span><span class ='unit'>").concat(translatedValue.unit || '', "</span></div>");
+  });
+} // Given vars passed as param, retrieves the selected city
+
+
+function getSelectedCity(vars, selectedVarName) {
+  var cityEnv = vars.filter(function (elem) {
+    return elem.name === selectedVarName;
+  });
+  var city = null;
+  if (cityEnv && cityEnv.length === 1) city = cityEnv[0].current.value;
+  return city;
+} // Access remote api and gives the coordinates from a city center based on NOMINATIM url server
+
+
+function getCityCoordinates(city_name) {
+  var url = NOMINATIM_ADDRESS.replace('<city_name>', city_name);
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    return {
+      latitude: data[0].lat,
+      longitude: data[0].lon
+    };
+  })["catch"](function (error) {
+    return console.error(error);
+  });
+}
+/*
+* Auxiliar functions
+*/
+// just for improve DRY
+
+
+function convertDate(time_) {
+  var time = new Date(time_);
+  var day = time.getDate();
+  var month = time.getMonth();
+  var year = time.getFullYear();
+  var hour = time.getHours() - 1;
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  var milliseconds = time.getMilliseconds();
+  return Date.UTC(year, month, day, hour + 1, minutes, seconds, milliseconds);
+}
+
+var geolocationOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 110
+};
+exports.geolocationOptions = geolocationOptions;
+//# sourceMappingURL=map_utils.js.map
